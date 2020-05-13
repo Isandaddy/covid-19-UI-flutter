810d267c73b26f1d31946bc9b5a4f209fdac6d96
Seung Tack Jo <jstack87215@gmail.com>
2020-05-13 23:16:37 +0900

    add Map

--- lib/main.dart
+++ lib/main.dart
@@ -162,6 +162,43 @@ class HomeScreen extends StatelessWidget {
                   ],
                 ),
               ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Spread of Virus",
+                    style: kTitleTextstyle,
+                  ),
+                  Text(
+                    "See Detail",
+                    style: TextStyle(
+                        color: kPrimaryColor, fontWeight: FontWeight.w600),
+                  )
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                padding: EdgeInsets.all(20),
+                height: 178,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 30,
+                        color: kShadowColor,
+                      )
+                    ]),
+                child: Image.asset(
+                  "assets/images/map.png",
+                  fit: BoxFit.contain,
+                ),
+              ),
             ],
           ),
         )
